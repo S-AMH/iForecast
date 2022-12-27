2a08b1399e4409156a9dca2ecfec398fbf7b0dcb
Seyed Amirmohammad Hasanli <hasanli@omrt.tech>
2022-12-27 19:19:30 +0100

    Add User Entity, Controller and Views

---
+++ iForecast/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using iForecast.Services.Cypher;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iForecast.Entities
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        [NotMapped]
+        private string _password = null!;
+        [Required]
+        public string Password { get { return _password; } set { _password = value; } }
+        [Required]
+        [DisplayName("Joined")]
+        public DateTime CreatedDate { get; set; }
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedDate = DateTime.Now;
+        }
+        public void SetPassword(string password)
+        {
+            _password = _password = HashEngine.HashText(password, Id);
+        }
+    }
+}
---
+++ iForecast/Services/Business/UserServices.cs
@@ -0,0 +1,56 @@
+﻿using iForecast.Entities;
+using iForecast.Services.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace iForecast.Services.Business
+{
+    public class UserServices
+    {
+        private readonly DataContext dataContext;
+        public UserServices(DataContext dataContext)
+        {
+            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+        public async Task Create(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            user.SetPassword(user.Password);
+            await dataContext.Users.AddAsync(user);
+            await dataContext.SaveChangesAsync();
+        }
+        public async Task Update(User _user)
+        {
+            if (_user == null)
+                throw new ArgumentNullException(nameof(_user));
+            User? user = await dataContext.Users.FindAsync(_user.Id);
+            if (user == null)
+                throw new KeyNotFoundException(nameof(_user.Id));
+            user.Email = _user.Email;
+            user.Name = _user.Name;
+            user.SetPassword(_user.Password);
+            await dataContext.SaveChangesAsync();
+        }
+        public async Task Delete(User _user)
+        {
+            if (_user == null)
+                throw new ArgumentNullException(nameof(_user));
+            User? user = await dataContext.Users.FindAsync(_user.Id);
+            if (user == null)
+                throw new KeyNotFoundException(nameof(_user.Id));
+            dataContext.Users.Remove(user);
+            await dataContext.SaveChangesAsync();
+        }
+        public async Task<ICollection<User>> ListAllUsers()
+        {
+            return await dataContext.Users.ToListAsync();
+        }
+        public async Task<User> GetUser(string id)
+        {
+            User? user = await dataContext.Users.FindAsync(id);
+            if (user == null)
+                throw new KeyNotFoundException(nameof(id));
+            return user;
+        }
+    }
+}
